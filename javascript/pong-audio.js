e06b22fea6c9c195cbf0a5f9b92a6ea1d01f1cfb
Elizabeth Adeeko <MichaelAdeyemo@my.unt.edu>
2021-06-24 15:42:25 -0500

    new background sound

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -49,7 +49,7 @@ soundArray.push(wallSound); //add sound to list of sounds
 export var paddleSound = new soundFile("ship_bells.mp3");
 soundArray.push(paddleSound);
 
-export var scoreSound = new soundFile("ocean_waves.mp3");
+export var scoreSound = new soundFile("ocean.mp3");
 soundArray.push(scoreSound);
 
 export var ambientSound = new soundFile("Adventure_Meme.mp3");
